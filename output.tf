3fe8e2947f5318302c6c0e80a57cf0df95ea4e35
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-04-22 08:47:40 +0200

    feat:(output): Added alarms to output

--- output.tf
+++ output.tf
@@ -13,3 +13,54 @@ output "target_group_arn_suffix" {
   value       = module.alb-ingress.target_group_arn_suffix
 }
 
+######### ALARMS ###########
+
+output "httpcode_target_3xx_count_cloudwatch_metric_alarm_id" {
+  value       = module.alb-target-group-alarms.httpcode_target_3xx_count_cloudwatch_metric_alarm_id
+  description = "Target Group 3xx count CloudWatch metric alarm ID"
+}
+
+output "httpcode_target_3xx_count_cloudwatch_metric_alarm_arn" {
+  value       = module.alb-target-group-alarms.httpcode_target_3xx_count_cloudwatch_metric_alarm_arn
+  description = "Target Group 3xx count CloudWatch metric alarm ARN"
+}
+
+output "httpcode_target_4xx_count_cloudwatch_metric_alarm_id" {
+  value       = module.alb-target-group-alarms.httpcode_target_4xx_count_cloudwatch_metric_alarm_id
+  description = "Target Group 4xx count CloudWatch metric alarm ID"
+}
+
+output "httpcode_target_4xx_count_cloudwatch_metric_alarm_arn" {
+  value       = module.alb-target-group-alarms.httpcode_target_4xx_count_cloudwatch_metric_alarm_arn
+  description = "Target Group 4xx count CloudWatch metric alarm ARN"
+}
+
+output "httpcode_target_5xx_count_cloudwatch_metric_alarm_id" {
+  value       = module.alb-target-group-alarms.httpcode_target_5xx_count_cloudwatch_metric_alarm_id
+  description = "Target Group 5xx count CloudWatch metric alarm ID"
+}
+
+output "httpcode_target_5xx_count_cloudwatch_metric_alarm_arn" {
+  value       = module.alb-target-group-alarms.httpcode_target_5xx_count_cloudwatch_metric_alarm_arn
+  description = "Target Group 5xx count CloudWatch metric alarm ARN"
+}
+
+output "httpcode_elb_5xx_count_cloudwatch_metric_alarm_id" {
+  value       = module.alb-target-group-alarms.httpcode_elb_5xx_count_cloudwatch_metric_alarm_id
+  description = "ELB 5xx count CloudWatch metric alarm ID"
+}
+
+output "httpcode_elb_5xx_count_cloudwatch_metric_alarm_arn" {
+  value       = module.alb-target-group-alarms.httpcode_elb_5xx_count_cloudwatch_metric_alarm_arn
+  description = "ELB 5xx count CloudWatch metric alarm ARN"
+}
+
+output "target_response_time_average_cloudwatch_metric_alarm_id" {
+  value       = module.alb-target-group-alarms.target_response_time_average_cloudwatch_metric_alarm_id
+  description = "Target Group response time average CloudWatch metric alarm ID"
+}
+
+output "target_response_time_average_cloudwatch_metric_alarm_arn" {
+  value       = module.alb-target-group-alarms.target_response_time_average_cloudwatch_metric_alarm_arn
+  description = "Target Group response time average CloudWatch metric alarm ARN"
+}
