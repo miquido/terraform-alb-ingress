3b5b684992f8cdabafce74b44991e0d46e6cdde2
Miquido Ops <mqtc@miquido.com>
2022-07-20 08:29:32 +0000

    chore(deps): bump alb-target-group-alarms from 0.16.1 to 0.17.0

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ module "alb-ingress" {
 }
 
 module "alb-target-group-alarms" {
-  source                                 = "git::https://github.com/cloudposse/terraform-aws-alb-target-group-cloudwatch-sns-alarms.git?ref=0.16.1"
+  source                                 = "git::https://github.com/cloudposse/terraform-aws-alb-target-group-cloudwatch-sns-alarms.git?ref=0.17.0"
   enabled                                = var.alb_target_group_alarms_enabled
   name                                   = var.name
   namespace                              = var.project
